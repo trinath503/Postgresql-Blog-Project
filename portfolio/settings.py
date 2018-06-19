d7d1386ab7c8439a37f6d143545b6a6ecad4abe6
trinath-xelpmoc <trinath.reddy@xelpmoc.in>
2018-06-19 16:07:53 +0530

    django-db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD': 'postgres-password',
-        'HOST':'localhost',
-        'port':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
