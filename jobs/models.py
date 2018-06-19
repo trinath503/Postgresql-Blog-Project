c19d4976431d4e7a9c56e96d12083a28a1b71d7d
trinath-xelpmoc <trinath.reddy@xelpmoc.in>
2018-06-19 11:38:44 +0530

    Superuser

--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
