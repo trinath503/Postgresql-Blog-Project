5da9b22ca7e132118579b4624312b01a443dab70
trinath-xelpmoc <trinath.reddy@xelpmoc.in>
2018-06-20 11:25:00 +0530

    ok

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs',
-    'blog',
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 
 ]
 
