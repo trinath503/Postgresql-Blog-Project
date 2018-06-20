9110450caab42bedda2593e4dc551094715ef1eb
trinath-xelpmoc <trinath.reddy@xelpmoc.in>
2018-06-20 17:17:05 +0530

    Final Blog

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from jobs import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home, name="home"),
     path('blogs/',include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
