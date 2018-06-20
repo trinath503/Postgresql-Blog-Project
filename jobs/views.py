42c582ffa891923a0dfe4317906673929c91089f
trinath-xelpmoc <trinath.reddy@xelpmoc.in>
2018-06-20 11:09:43 +0530

    Working

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Job
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html',{'jobs':jobs})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs.apps.JobsConfig',
-    'blog.apps.BlogConfig',
+    'jobs',
+    'blog',
 
 ]
 
